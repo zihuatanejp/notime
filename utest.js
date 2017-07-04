b8da268a605b138cdadaf9c4ed519444ccd7e389
lijian <aimilixi@foxmail.com>
2017-07-04 18:31:55 +0800

    补全基本库 大数运算方法 fixed 减法的bug
    Signed-off-by: lijian <aimilixi@foxmail.com>

--- utest.js
+++ utest.js
@@ -1,1 +1,74 @@
 'use strict';
+
+var nt = require('./notime');
+var ut = nt.util;
+
+// var tp1 = nt.util.bnplus('11111111111111111','99999999999999988888888');
+// console.log(tp1);
+
+/*
+var tp2 = ut.bnmultip('999','99');
+// console.log(tp2);  //should 98901  ok
+
+var tp3 = ut.bnmultip('111','11');
+// console.log(tp3);  // should 1221 ok
+
+var tp4 = ut.bnmultip('111','111');
+// console.log(tp4);  // should 12321 ok
+
+var tp5 = ut.bnmultip('9999','9999');
+// console.log(tp5);  // should 99980001 ok
+
+var tp6 = ut.bnmultip('234','6789');
+// console.log(tp6);  // should 1588626 ok
+
+var tp7 = ut.bnmultip('234','6289');
+console.log(tp7);  // should 1471626 ok
+
+*/
+
+/*
+var tp8 = ut.bndivis('999','99');
+console.log(tp8);  // should 10 ok
+
+var tp9 = ut.bndivis('100','99');
+console.log(tp9);  // should 1 ok
+
+var tp10 = ut.bndivis('1000','1');
+console.log(tp10);  // should 1000 ok
+
+var tp11 = ut.bndivis('1000','2');
+console.log(tp11);  // should 500 ok
+
+*/
+
+/*
+
+var tp12 = ut.bnmod('100','99');
+console.log(tp12); // should 1 ok
+
+var tp13 = ut.bnmod('1000','2');
+console.log(tp13);  // should 0 ok
+
+var tp14 = ut.bnmod('964','22');
+console.log(tp14);  // should 18 ok
+
+var tp15 = ut.bnminus('2','2');
+console.log(tp15);  // should 0  res:''
+
+var tp16 = ut.bnminus('999','999');
+console.log(tp16); // should 0    res:00  
+// / 已解决两数相等时不进行后续减法运算
+
+var tp17 = ut.bnminus('100','99');
+console.log(tp17);  // should 1  res:01
+
+var tp17 = ut.bnminus('1000','99');
+console.log(tp17);  // should 901  res:901 ok
+
+var tp17 = ut.bnminus('1000','999');
+console.log(tp17);  // should 1  res:001
+///解决方案: 得到最终结果后处理前面全是0的情况,去掉前面的0 ok
+
+*/
+
---
+++ util.js
@@ -0,0 +1,366 @@
+'use strict';
+
+module.exports = {
+	bnplus:bnplus,
+	bnminus:bnminus,
+	bnmultip:bnmultip,
+	bndivis:bndivis,
+	bnmod:bnmod,
+	bnisnega:bnisnega,
+	bnabscomp:bnabscomp
+};
+
+// 大数加法
+function bnplus(a1,a2){
+	if ( a1.length > a2.length ){
+		var tp1 = a1;
+		a1 = a2;
+		a2 = tp1;
+	}
+	var ar1 = a1.split('');
+	var ar2 = a2.split('');
+	var carryflag = 0; //进位标志
+	var rstr = ''; //输出的计算结果str
+	var secnum; //数组项的结果
+	var secstr; //数组项结果的str
+	(function () {
+	var tp2 = 0; //当前计算的是倒数第几个数
+	for(var i = (a1.length-1);i>=0;i-- ){
+		++tp2;	
+		(function () {
+		var tp3 = 0;
+		var tp4 = false; //a1是否已完
+		var tp5 = false; // 是否已加过a1的最后一个
+		for(var j = (a2.length-1);j>=0;j--){
+			++tp3;
+			//对应倒数位上的数计算
+			if(tp2 == tp3){
+				if(i==0){
+					tp4 = true;
+				}
+				else{
+					secnum = parseInt(ar1[i])+parseInt(ar2[j])+carryflag;
+					secstr = secnum.toString();
+					if(secstr.length>1){
+						secstr = secstr.substr(-1);
+						carryflag = 1;
+					}
+					else{
+						carryflag = 0;
+					}
+					rstr = secstr+rstr;
+				}
+			}
+			// 如果a1已完
+			if(tp4){
+				if(tp5){
+					secnum = parseInt(ar2[j])+carryflag;
+				}
+				else{
+					secnum = parseInt(ar1[i])+parseInt(ar2[j])+carryflag;
+					tp5 = true;
+				}
+				secstr = secnum.toString();
+				if(j == 0){ //如果a2也到了最后一位
+					rstr = secstr+rstr;
+				}
+				else{
+					if(secstr.length>1){
+						secstr = secstr.substr(-1);
+						carryflag = 1;
+					}
+					else{
+						carryflag = 0;
+					}
+					rstr = secstr+rstr;
+				}	
+			}
+		}
+		})();
+	}
+	})();
+	return rstr;
+}
+
+
+//大数减法
+function bnminus(a1,a2) {
+
+	//两数绝对值相等,则不用后续计算
+	if( bnabscomp(a1,a2) =='eq'){
+		return '0';
+	}
+	var ar1 = a1.split('');
+	var ar2 = a2.split('');
+	var carryflag = 0; //退位标志
+	var rstr = '';
+	var secnum;
+	var secstr;
+	(function () {
+		var tp2 = 0;
+		for(var i = (a2.length-1);i>=0;i-- ){
+			++tp2;
+			(function () {
+				var tp3 =0;
+				var tp4 = false; //a2是否已完
+				var tp5 = false; // 是否已减过a2 的最后一个
+				var tpa1,tpa2; // a1的倒数位数 和a2的倒数位数
+				for(var j = (a1.length-1);j>=0;j--){
+					++tp3;
+					if(tp3 == tp2){ 
+						if(i==0){
+							tp4 = true;
+						}
+						else{
+							tpa1 = parseInt(ar1[j])-carryflag;
+							tpa2 = parseInt(ar2[i]);
+							if(tpa1>=tpa2){
+								secnum = tpa1-tpa2;
+								carryflag = 0;
+							}
+							else{
+								secnum = tpa1+10-tpa2;
+								carryflag = 1;
+							}
+							secstr = secnum.toString(); //console.log(secstr);
+							rstr = secstr+rstr;
+						}
+					}
+					if(tp4){	
+						tpa1 = parseInt(ar1[j])-carryflag;					
+						tpa2 = parseInt(ar2[i]); 
+						if(tp5){
+							if(tpa1<0){
+								secnum = 9;
+								carryflag = 1;
+							}
+							else{
+								secnum = tpa1;
+								carryflag = 0;
+							}
+						}
+						else{							
+							if(tpa1>=tpa2){ 
+								secnum = tpa1-tpa2;
+								carryflag = 0;
+							}
+							else{
+								secnum = tpa1+10-tpa2;
+								carryflag = 1;
+							}
+							tp5 = true;
+						}
+
+						secstr = secnum.toString();
+						// 如果a1的最后一位为0
+						if(j==0){
+							if(secstr != '0'){
+								rstr = secstr+rstr;
+							}
+						}
+						else{
+							rstr = secstr+rstr;
+						}
+					}
+				}
+			})();
+		}
+	})();
+	// 是否是前面全是零的情况
+	if( (rstr.length>1)&&( rstr.charAt(0)=='0' ) ){
+		var ind =0;
+		for (var k = 0; k < rstr.length; k++) {
+			if( rstr.charAt(k) != '0'){
+				ind = k;
+				break;
+			}
+		}
+		rstr = rstr.substr(ind);
+	}	
+	return rstr;
+}
+
+// 大数乘法  //不支持乘以小数
+function bnmultip(a1,a2){
+	// 大数在前 小数在后
+	var comp = bnabscomp(a1,a2);
+	if(comp == 'no'){
+		var tp1 = a1;
+		a1 = a2;
+		a2 = tp1;
+	}
+
+	// 切割为数组
+	var ar1 = a1.split('');
+	var ar2 = a2.split('');
+
+	var carryflag = 0; //进位后加的数
+	var rstr = ''; //最终得到的结果
+	var secstr = '';//每完成一段 1对n后最终得到的结果str
+	var cellnum; // 每个1对1的乘积
+	var cellstr; // 每个1对1的乘积的str
+	var tp2 =0;//计数当前是小数的第几个数进行乘了,用来判断补零
+
+	// 开始从小数的最后位乘起
+	(function () {
+		for (var i = (ar2.length-1);i>=0;i--) {
+			++tp2;
+			// 每次进来清掉重置上一次的片段值
+			secstr = '';
+
+			for(var j = (ar1.length-1);j>=0;j--){
+				// 如果小数的该位数为0 ,则secstr的结果直接为0
+				if( parseInt(ar2[i]) ==0 ){
+					secstr = '0';
+				}
+				else{
+					cellnum = parseInt(ar2[i])*parseInt(ar1[j])+parseInt(carryflag);
+					cellstr = cellnum.toString();
+					carryflag = 0;
+					if(cellstr.length>1){
+						//乘到大数的首位了，或者没有
+						if(j==0){
+							carryflag = 0;
+						}
+						else{
+							carryflag = cellstr.substr(0,1);
+							cellstr = cellstr.substr(-1);
+						}
+					}
+					secstr = cellstr+secstr;
+					//运行到该片段最后一次乘运算时，进行补零
+					if(j==0){
+						for (var k = 0; k < (tp2-1); k++) {
+							secstr = secstr+'0';
+						}
+					}
+				}
+			}
+			
+
+			// 0就不用加了
+			if(secstr!='0'){
+				if(rstr){
+					rstr = bnplus(rstr,secstr);					
+				}
+				else{
+					rstr = secstr;
+				}
+				
+			}
+		}
+	})();
+
+	return rstr;
+}
+
+// 大数除法 //返回整数结果 不支持被除数小于除数
+function bndivis(a1,a2) {
+	var comp = bnabscomp(a1,a2); 
+	if(comp=='no'){
+		return false;
+	}
+	if(comp =='eq'){
+		return '1';
+	}
+	var flag = true; 
+	var cnt = 0;
+	var tp; // 减去后的结果值,临时变量
+	while(flag){
+		// 判断第一次 和以后的许多次
+		if(!tp){
+			tp = bnminus(a1,a2);
+		}
+		else{
+			tp = bnminus(tp,a2);
+		}
+		
+		++cnt;
+		//减去后比较是否已小于，小于则跳出，否则进下一次
+		if( bnabscomp(tp,a2) == 'no' ){
+			flag = false;
+		}
+	}
+	return cnt;
+}
+
+// 大数取余 不支持被除数小于除数
+function bnmod(a1,a2) {
+	var comp = bnabscomp(a1,a2); 
+	if(comp=='no'){
+		return false;
+	}
+	if(comp =='eq'){
+		return '1';
+	}
+	var flag = true;
+	var tp; // 减去后的结果值,临时变量
+	while(flag){
+		// 判断第一次 和以后的许多次
+		if(!tp){
+			tp = bnminus(a1,a2);
+		}
+		else{
+			tp = bnminus(tp,a2);
+		}
+		// console.log(tp);
+		//减去后比较是否已小于，小于则跳出，否则进下一次
+		if( bnabscomp(tp,a2) == 'no' ){
+			flag = false;
+		}
+		if( bnabscomp(tp,a2) == 'eq' ){
+			tp = '0';
+			flag = false;
+		}
+	}
+	return tp;
+}
+
+// 判斷是否为负数
+function bnisnega(str){
+	var res = false;
+	if( str.charAt(0) == '-' ){
+		res = true;
+	}
+	return res;
+}
+
+
+// 比较两数的绝对值大小 //a1>a2 yes  //a1<a2  no  //两数相等 eq
+function bnabscomp(a1,a2){
+	var res;
+	if( bnisnega(a1) ){
+		a1 = a1.slice(1);
+	}
+	if( bnisnega(a2) ){
+		a2 = a2.slice(1);
+	} 
+	if(a1.length>a2.length){
+		res = 'yes';
+	}
+	if(a1.length<a2.length){
+		res = 'no';
+	}
+	if(a1.length == a2.length){
+		var ar1 = a1.split('');
+		var ar2 = a2.split('');
+		var tpa1,tpa2; 
+		for(var i =0;i<(ar1.length);i++){
+			tpa1 = parseInt(ar1[i]);
+			tpa2 = parseInt(ar2[i])
+			if( tpa1 > tpa2 ){
+				res = 'yes';
+				break;
+			}
+			if(tpa1 < tpa2){
+				res = 'no';
+				break;
+			}
+			if(tpa1 == tpa2){
+				res = 'eq';
+				continue;
+			}
+		}
+	}
+	return res;
+}
